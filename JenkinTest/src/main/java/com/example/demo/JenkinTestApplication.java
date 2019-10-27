ce62e8fbc7f22cc3c0a798fdbde81947c3b157fa
jnanapalai <jnana335@gmail.com>
2019-10-27 23:23:21 +0530

    rest api

--- JenkinTest/src/main/java/com/example/demo/JenkinTestApplication.java
+++ JenkinTest/src/main/java/com/example/demo/JenkinTestApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -10,6 +11,11 @@ public class JenkinTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinTestApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String HelloWorld()
+	{
+		return "HelloWorld";
 	}
 
 }
